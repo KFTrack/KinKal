c27421c4f21e57761c37571f35d837892dc82404
Ray Culbertson <rlc@fnal.gov>
2017-12-14 14:00:16 -0600

    move setup.sh to v1_02_07

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ fi
 # The following are used by the install script.
 # They must be maintained by hand.
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_05
+export PACKAGE_VERSION=v1_02_07
 # Done parsing and checking arguments
 
 # The target directory for the build is the directory from which the
