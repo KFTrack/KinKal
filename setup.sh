28926203017b6bc6b52a1634627247a721250cf8
Rob Kutschke <kutschke@fnal.gov>
2018-08-28 11:57:46 -0500

    Update ups products to match to art v2_11_01, with gcc v7.3.0

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ fi
 # The following are used by the install script.
 # They must be maintained by hand.
 export PACKAGE_NAME=BTrk
-export PACKAGE_VERSION=v1_02_11
+export PACKAGE_VERSION=v1_02_12
 # Done parsing and checking arguments
 
 # The target directory for the build is the directory from which the
@@ -49,14 +49,14 @@ echo "Debug level:        " ${DEBUG_LEVEL}
 
 # These are a matched pair and must be kept in sync by hand.
 # See: https://cdcvs.fnal.gov/redmine/projects/cet-is-public/wiki/AboutQualifiers
-setup -B gcc v6_4_0
-export COMPILER_CODE=e15
+setup -B gcc v7_3_0
+export COMPILER_CODE=e17
 
 # Choose versions of the remaining UPS products.
 qualifiers=+${COMPILER_CODE}:+${DEBUG_LEVEL}
 
-setup -B clhep v2_3_4_5c -q${qualifiers}
-setup -B root v6_12_04e -q${qualifiers}
+setup -B clhep v2_3_4_6  -q${qualifiers}
+setup -B root  v6_12_06a -q${qualifiers}
 setup -B scons v3_0_1  -q p2714b
 # Only used inside scripts/install.sh, to get the flavor of the build platform.
 setup cetpkgsupport
