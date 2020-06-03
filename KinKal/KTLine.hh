b7cc885d9a892f9d8155a27a0d9d72423d084e6c
Ryunosuke O'Neil <roneil@fnal.gov>
2020-06-04 00:16:26 +0100

    should inherit typedefs from TLine

--- KinKal/KTLine.hh
+++ KinKal/KTLine.hh
@@ -22,9 +22,9 @@ namespace KinKal {
   class KTLine :  public TLine {
     public:
       constexpr static ParamIndex t0Index() { return t0_; }
-      typedef ROOT::Math::SVector<double,npars_> PDER; // derivative of parameters type
-      typedef PData<npars_> PDATA; // Data payload for this class
-      typedef typename PDATA::DVEC DVEC; // derivative of parameters type
+      //typedef ROOT::Math::SVector<double,npars_> PDER; // derivative of parameters type
+      //typedef PData<npars_> PDATA; // Data payload for this class
+      //typedef typename PDATA::DVEC DVEC; // derivative of parameters type
       static std::string const& trajName();
       // This also requires the nominal BField, which can be a vector (3d) or a scalar (B along z)
       KTLine(Vec4 const& pos, Mom4 const& mom, int charge, Vec3 const& bnom, TRange const& range=TRange());
