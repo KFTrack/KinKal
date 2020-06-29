41e3579c8a755c4b1b926d000d8c887a453196cc
Ryunosuke O'Neil <roneil@fnal.gov>
2020-06-29 17:00:40 +0100

    CI test

---
+++ .ci-setup.sh
@@ -0,0 +1,11 @@
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+setup mu2e
+setup root v6_18_04d -f Linux64bit+3.10-2.17 -q e19:prof
+setup cmake v3_9_0
+
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+
+make -j 8
+
+make test
