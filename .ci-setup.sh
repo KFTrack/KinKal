9d8751cdccc607dcc5158ba5bc5972a39049c82c
Ryunosuke O'Neil <roneil@fnal.gov>
2020-06-29 18:28:56 +0100

    cleanup ci scripts

--- .ci-setup.sh
+++ .ci-setup.sh
@@ -1,16 +1,11 @@
-
-
-#yum -y install http://ftp.scientificlinux.org/linux/scientific/7x/contexts/fermilab/x86_64/yum-conf-context-fermilab-1.0-6.el7.noarch.rpm
-#sleep 1
-#yum -y upgrade
-yum -y install make base-devel glibc-devel freetype-devel # redhat-lsb-core
+yum -y install make base-devel glibc-devel freetype-devel
 
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup mu2e
 setup root v6_18_04d -f Linux64bit+3.10-2.17 -q e19:prof
 setup cmake v3_9_0
 
-rm -rf build || echo "build exists"
+rm -rf build || echo ""
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 
