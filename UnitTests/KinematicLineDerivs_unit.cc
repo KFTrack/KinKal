d8809dea6b3afbdb13c5faa580bc79b6eb00bdc3
sophieMu2e <sophie@fnal.gov>
2020-09-11 18:13:35 -0500

    moved tests

---
+++ UnitTests/KinematicLineDerivs_unit.cc
@@ -0,0 +1,8 @@
+/*
+  Original Author: S Middleton 2020
+*/
+#include "KinKal/KinematicLine.hh"
+#include "UnitTests/KTrajDerivs_test.hh"
+int main(int argc, char **argv) {
+  return test<KinematicLine>(argc,argv);
+}
