94a99376a002c4f492010d0636263fc874e0d514
sophieMu2e <sophie@fnal.gov>
2020-09-11 18:21:23 -0500

    last commit

--- UnitTests/KinematicDerivs_unit.cc
+++
@@ -1,8 +0,0 @@
-/*
-  Original Author: S Middleton 2020
-*/
-#include "KinKal/KTLine.hh"
-#include "UnitTests/KTrajDerivs_test.hh"
-int main(int argc, char **argv) {
-  return test<KTLine>(argc,argv);
-}
