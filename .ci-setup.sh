971aa6caafd69e1855609fc8035052ffd25c8b69
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-13 02:22:32 +0100

    use stock python 3

--- .ci-setup.sh
+++ .ci-setup.sh
@@ -1,10 +1,10 @@
-yum -y install make base-devel glibc-devel freetype-devel
+yum -y install make base-devel glibc-devel freetype-devel python3
 
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup mu2e
 setup root v6_18_04d -f Linux64bit+3.10-2.17 -q e19:prof
 
-python -m pip install scons
+python3 -m pip install scons
 
 rm -rf build || echo ""
 mkdir build && cd build
