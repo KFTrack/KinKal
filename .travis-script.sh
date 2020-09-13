f736d85659396a9705fa244b4e3d05d91133465f
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-13 18:58:34 +0100

    print output for failed tests on macOS

--- .travis-script.sh
+++ .travis-script.sh
@@ -3,20 +3,12 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cmake .. -DCMAKE_BUILD_TYPE=Release
     
     make -j 8
-    make test
-
-#    source ../scripts/newBuild.sh prof 
-#    source setup.sh
-
-#    scons -j 4
-#    scons test
-
+    env CTEST_OUTPUT_ON_FAILURE=1 make test
 else
-
+    # set up SL7 docker container
     cd .. 
 
     docker run --name KinKalCI -v /cvmfs:/cvmfs:ro,shared -v "$(pwd)"/KinKal:/KinKal -it -d scientificlinux/sl:7
     docker exec -ti KinKalCI /bin/bash -c "cd /KinKal && source .ci-setup.sh"
-
 fi
 
