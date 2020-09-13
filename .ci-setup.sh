0150e85b7bbf7f7ef50f8ce9cfeba007d7c8984e
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-13 02:21:15 +0100

    install scons by pip

--- .ci-setup.sh
+++ .ci-setup.sh
@@ -3,7 +3,8 @@ yum -y install make base-devel glibc-devel freetype-devel
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup mu2e
 setup root v6_18_04d -f Linux64bit+3.10-2.17 -q e19:prof
-scons v3_1_2 -q p383b 
+
+python -m pip install scons
 
 rm -rf build || echo ""
 mkdir build && cd build
