438813c90862a18a25ed190a17387f8e69655509
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-13 03:25:34 +0100

    organise travis logfile

--- .ci-setup.sh
+++ .ci-setup.sh
@@ -1,3 +1,6 @@
+echo -en 'travis_fold:start:SLF7ContainerSetup\r'
+echo "SLF7 Container Setup"
+
 yum -y install epel-release
 yum -y install make base-devel glibc-devel freetype-devel xxhash-devel xxhash-libs libcurl libcurl-devel libzstd-devel libzstd
 
@@ -7,6 +10,12 @@ source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup mu2e
 setup root v6_20_06 -f Linux64bit+3.10-2.17 -q e19:p383b:prof
 #setup -B scons v3_1_2 -q +p383b
+
+
+echo -en 'travis_fold:end:SLF7ContainerSetup\r'
+
+echo -en 'travis_fold:start:Build\r'
+echo "Build"
 
 rm -rf build || echo ""
 mkdir build && cd build
@@ -24,4 +33,8 @@ setup cmake v3_17_3
 cmake .. -DCMAKE_BUILD_TYPE=Release
 
 make -j 8
+
+echo -en 'travis_fold:end:Build\r'
+
 make test 
+
