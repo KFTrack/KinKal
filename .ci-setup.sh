f5439bd815b789d0cf1e48a05e45a2fb22966aea
Ryunosuke O'Neil <roneil@fnal.gov>
2020-09-13 02:27:43 +0100

    try ups one more time

--- .ci-setup.sh
+++ .ci-setup.sh
@@ -1,10 +1,11 @@
 yum -y install make base-devel glibc-devel freetype-devel python3
 
+#python3 -m pip install scons
+
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup mu2e
 setup root v6_18_04d -f Linux64bit+3.10-2.17 -q e19:prof
-
-python3 -m pip install scons
+setup -B scons v3_1_2 -q +p383b
 
 rm -rf build || echo ""
 mkdir build && cd build
