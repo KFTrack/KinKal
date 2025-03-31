a256b1a7d31ee15c0e2642555ad719d36bc96468
David Nathan Brown <dave_brown@lbl.gov>
2025-03-31 12:24:34 -0700

    Modify traj ranges based on domains to avoid rare FP collision

--- Fit/Track.hh
+++ Fit/Track.hh
@@ -280,17 +280,16 @@ namespace KinKal {
       // loop until we're either out of this domain or the piece is out of this domain
       while(dtime < domain->end()){
         // find the nearest piece of the traj
-        auto index = fittraj_->nearestIndex(dtime);
+        static double epsilon(1e-10);
+        auto index = fittraj_->nearestIndex(dtime+epsilon); // make sure step to the next segment
         auto const& oldpiece = *fittraj_->pieces()[index];
         // create a new piece
         KTRAJ newpiece(oldpiece,bf,dtime);
-        // set the range as needed
-        double endtime = (index < fittraj_->pieces().size()-1) ? std::min(domain->end(),oldpiece.range().end()) : domain->end();
+        // set the range for this piece, making sure it is non-zero
+        double endtime = (index < fittraj_->pieces().size()-1) ? std::max(std::min(domain->end(),oldpiece.range().end()),dtime+epsilon) : domain->end();
         newpiece.range() = TimeRange(dtime,endtime);
         newtraj->append(newpiece);
-        // update the time
-        static double epsilon(1e-10);
-        dtime = newpiece.range().end()+epsilon; // to avoid boundary
+        dtime = newpiece.range().end();
       }
     }
     // update all effects to reference this trajectory
